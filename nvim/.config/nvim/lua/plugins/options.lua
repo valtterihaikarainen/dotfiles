72d73ac429c97839c4950c5cc98897d5d8b3c334
Valtteri Haikarainen <valtterihaikarainen@Valtteris-MacBook-Air.local>
2025-05-27 17:33:47 +0300

    fixed nvim tracking

---
+++ nvim/.config/nvim/lua/plugins/options.lua
@@ -0,0 +1,8 @@
+vim.cmd("colorscheme catppuccin-macchiato") -- set color theme
+
+-- ensure auto-session restores local options (filetypes, highlights, etc.)
+vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
+vim.opt.termguicolors = true --bufferline
+require("bufferline").setup{} --bufferline
+
+
