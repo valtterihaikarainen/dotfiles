26e7f480cc371b3eda1a0406532588e0ad75c133
Valtteri Haikarainen <valtterihaikarainen@Valtteris-MacBook-Air.local>
2025-06-08 15:29:37 +0300

    removed old nvim configs

--- stow/nvim-old/.config/nvim/init.lua
+++
@@ -1,6 +0,0 @@
-require('options')
-require('keymaps')
-require('plugins.lazy')
-require('plugins.keymaps')
-require('plugins.options')
-
--- stow/nvim-old/.config/nvim/lua/plugins/options.lua
+++
@@ -1,8 +0,0 @@
-vim.cmd("colorscheme gruvbox") -- set color theme
-
--- ensure auto-session restores local options (filetypes, highlights, etc.)
-vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
-vim.opt.termguicolors = true --bufferline
-require("bufferline").setup{} --bufferline
-
-
